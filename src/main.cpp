7addd30cc1e249b03faf129e5055fefe61cd24e1
kdko <ko.kidong@gmail.com>
2021-01-05 14:02:08 +0000

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,25 @@
 #include <iostream>
-#include <ctime> 
+#include <array>
 
 using namespace std; 
 
-const int Cities = 5;
-const int Years = 4;
 int main() 
 {    
-    const char * cities[Cities] = 
-    {
-        "Seoul",
-        "Jeju",
-        "Busan",
-        "Gangneung",
-        "Daegu"
-    }; 
+    int line=0;  
+    cout << "몇 줄 할겁니까? ";
+    cin >> line; 
 
-    int maxtemps[Years][Cities] =
-    {
-        {35,32,33,36,35},
-        {33,32,34,35,31},
-        {33,34,32,35,34},
-        {36,35,34,37,35}
-    }; 
-
-    cout << "2009년부터 2012년까지의 연중 최고 온도 \n\n";
-    for (int city = 0; city < Cities; city++)
-    {
-        cout << cities[city] <<": \t";
-        for (int year=0; year < Years; year++)
+    for (int i=0; i < line; i++)
+    {  
+        for (int j=i; j < line-1; j++) 
         {
-            cout << maxtemps[year][city] << "\t";
-        }
-        cout << "\n";
+            cout  << "-"; 
+        } 
+        for (int k=line-i ; k <line+1 ; k++)
+        {
+            cout  << "*";
+        } 
+        cout << endl;
     }
     return 0;
 }
