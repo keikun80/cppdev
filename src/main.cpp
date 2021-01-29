6c403eb89c3de853bf6d77f6e7cea7bc5d1432e1
kdko <ko.kidong@gmail.com>
2021-01-29 09:35:47 +0000

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,21 @@
 #include <iostream>
-#include <fstream>
+#include <cctype> 
 
-using namespace std; 
-
+using namespace std;
 int main()  
-{ 
-    char automobile[50];
-    int year;
-    double a_price;
-    double d_price;
-
-    ofstream outFile;
-    outFile.open("carinfo.txt");
-
-    cout << "자동차 메이커와 차종을 입력하세요 :";
-    cin.getline(automobile, 50);
-    cout << "연식을 입력하세요 : ";
-    cin >> year;
-    cout << "구입가를 입력하세요 : ";
-    cin >> a_price; 
-    d_price = a_price * 0.913;
-
-    cout << fixed;
-    cout.precision(2);
-    cout.setf(ios_base::showpoint);
-    cout << "차종 :" << automobile <<"\n";
-    cout << "연식 :" << year <<"\n";  
-    cout << "구매가 : " << a_price << "\n";
-    cout << "햔재가 : " << d_price << "\n";
-    
-    outFile << fixed;
-    outFile.precision(2);
-    outFile.setf(ios_base::showpoint);
-    outFile << "차종 :" << automobile <<"\n";
-    outFile << "연식 :" << year <<"\n";  
-    outFile << "구매가 : " << a_price << "\n";
-    outFile << "햔재가 : " << d_price << "\n";
+{   
+    char ch ;
+    cout << "Enter character: "; 
+    cin.get(ch);
+    while (true)
+    { 
+        if (isupper(ch))
+            cout.put(tolower(ch)); 
+        else if(islower(ch))
+            cout.put(toupper(ch)); 
+        else 
+            cout << ch << endl;
+        cin.get(ch);
+    }
     return 0;
 }
